27fe323c51e7a41067f9a533eaf4e665778a5cda
hejny <me@pavolhejny.com>
2018-03-08 16:11:01 +0100

    createSimpleTool

--- src/world/DrawingTools/PathDrawingToolFactory.ts
+++ src/world/DrawingTools/PathDrawingToolFactory.ts
@@ -13,7 +13,7 @@ export default class {
         return new PathDrawingTool(
             this._world,
             {
-                transformPath: this.transformPath,
+                transformPath: this.transformPathGrid,
                 modifySurfacePoint: (point: BABYLON.Vector3, center: DrawingPoint, tool: PathDrawingTool) => point,
                 tessalationInLength: 0.02,
                 tessalationInRadius: 7,
@@ -38,6 +38,43 @@ export default class {
             return drawingPoint;
 
         })
+
+
+    }
+
+
+    transformPathGrid(path: DrawingPoint[]): DrawingPoint[] {
+
+
+        const newPath: DrawingPoint[] = [];
+
+        for (let point of path) {
+
+            point = point.clone();
+            point.position.x = Math.round(point.position.x * 10) / 10;
+            point.position.y = Math.round(point.position.y * 10) / 10;
+            point.position.z = Math.round(point.position.z * 10) / 10;
+            newPath.push(point);
+
+
+        }
+
+        return newPath;
+
+        /*return path.reduce((path,drawingPoint,index) => {
+
+            drawingPoint = drawingPoint.clone();
+
+
+            if(Math.random()<.1){
+                path.push(drawingPoint);
+
+            }
+
+
+            return path;
+
+        },[]);*/
 
 
     }
